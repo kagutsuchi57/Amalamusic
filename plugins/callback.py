9deddcba03bbb64d44a5f1c413ecc25a71f786d2
Telugu coders <107198990+Telugucoders@users.noreply.github.com>
2022-06-30 13:25:04 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -230,7 +230,7 @@ async def info(_, query: CallbackQuery):
 
 
 @Client.on_callback_query(filters.regex("pausevc"))
-async def pausevc(_, query: CallbackQuery):
+async def pausevc(_, query: CallbackQuery, message: Message):
     if query.message.sender_chat:
         return await query.answer("ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ !\n\n» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.")
     a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
